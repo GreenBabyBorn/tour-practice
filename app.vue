6f869425e205b4098a8b2edd054135b9e836eb71
greenbabyborn <greenbabyborn.com>
2023-06-16 00:33:02 +0300

    init

---
+++ app.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>
+    <!-- Markup shared across all pages, ex: NavBar -->
+    <AppHeader></AppHeader>
+    <NuxtPage />
+  </div>
+</template>
+
+<script setup lang="ts"></script>
